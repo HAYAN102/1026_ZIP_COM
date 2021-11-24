4089edc5a8da2549c3f5ea4d7fdf85cf1d2b2d48
HAYAN102 <91953631+HAYAN102@users.noreply.github.com>
2021-11-24 21:03:20 +0900

    1124_버튼넣기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from "./logo.svg";
+import "antd/dist/antd.css";
 import "./App.css";
+import { Button } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
 
 function App() {
   return (
-    <div id="header">
-      <span>쟈쟌~~~~</span>
+    <div>
+      <div id="header">
+        <span>쟈쟌~~~~</span>
+      </div>
+      <div id="uploadButton">
+        <Button type="primary" ghost>
+          <UploadOutlined />
+          이미지 업로드 하기
+        </Button>
+      </div>
     </div>
   );
 }
