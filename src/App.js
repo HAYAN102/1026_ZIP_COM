0738f456d91994dcda515788c2da89bb31675de3
HAYAN102 <91953631+HAYAN102@users.noreply.github.com>
2021-11-25 21:38:44 +0900

    1125_업로드페이지로연결

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import "antd/dist/antd.css";
+import React from "react";
 import "./App.css";
 import { Button } from "antd";
 import { UploadOutlined } from "@ant-design/icons";
+import {
+  useHistory,
+  Link,
+  Switch,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import UploadPage from "./upload/index.js";
 
 function App() {
+  const history = useHistory();
   return (
     <div>
-      <div id="header">
-        <span>쟈쟌~~~~</span>
-      </div>
+      <Link to="/">
+        <div id="header">
+          <span>쟈쟌~~~~</span>
+        </div>
+      </Link>
       <div id="uploadButton">
-        <Button type="primary" ghost>
-          <UploadOutlined />
+        <Button
+          type="primary"
+          ghost
+          onClick={() => {
+            history.push("/upload");
+          }}
+          icon={<UploadOutlined />}
+        >
           이미지 업로드 하기
         </Button>
       </div>
+
+      <Switch>
+        <Route exact={true} path="/upload">
+          <UploadPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/upload/index.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import { useHistory } from "react-router-dom";
+
+function Uploadpage() {
+  const history = useHistory();
+
+  return (
+    <div>
+      <span>업로드페이지</span>;
+    </div>
+  );
+}
+
+export default Uploadpage;
