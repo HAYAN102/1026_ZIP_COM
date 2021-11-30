6ee9943d87d226bc8e99102ed7a6281461fe97b7
HAYAN102 <91953631+HAYAN102@users.noreply.github.com>
2021-11-30 21:14:23 +0900

    1130_post요청

--- src/main/index.js
+++ src/main/index.js
@@ -8,6 +8,19 @@ function MainPage() {
   const history = useHistory();
   const [banners, setBanners] = React.useState([]);
 
+  React.useEffect(function () {
+    axios
+      .get("https://69b99504-0bce-4fff-9de0-e95e1e36f9dc.mock.pstmn.io/banners")
+      .then(function (result) {
+        console.log(result);
+        const imageData = result.data.imageUrl;
+        console.log(imageData);
+      })
+      .catch(function (error) {
+        console.error("에러발생 : ", error);
+      });
+  });
+
   return (
     <div id="banners">
       <div id="uploadButton">
--- src/upload/index.js
+++ src/upload/index.js
@@ -1,27 +1,77 @@
+import "./index.css";
 import axios from "axios";
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import { Form, Input, Button, message, Upload } from "antd";
 
-function Uploadpage() {
+function UploadPage() {
+  const [imageUrl, setImageUrl] = useState(null);
   const history = useHistory();
-  React.useEffect(function () {
+
+  const onSubmit = (values) => {
     axios
-      .get("https://69b99504-0bce-4fff-9de0-e95e1e36f9dc.mock.pstmn.io/banners")
-      .then(function (result) {
+      .post(
+        "https://69b99504-0bce-4fff-9de0-e95e1e36f9dc.mock.pstmn.io/banners",
+        {
+          title: values.description,
+          imageUrl: imageUrl,
+        }
+      )
+      .then((result) => {
         console.log(result);
-        const imageData = result.data.imageUrl;
-        console.log(imageData);
+        history.replace("/");
       })
-      .catch(function (error) {
-        console.error("에러발생 : ", error);
+      .catch((error) => {
+        console.error(error);
+        message.error(`에러가 발생했습니다. %{error.message}`);
       });
-  });
+  };
+
+  const onChangeImage = (info) => {
+    if (info.file.status === "uploading") {
+      return;
+    }
+    if (info.file.status === "done") {
+      const response = info.file.response;
+      const imageUrl = response.imageUrl;
+      setImageUrl(imageUrl);
+    }
+  };
 
   return (
     <div>
       <span>업로드페이지</span>;
+      <Form id="upload">
+        <Upload
+          name="image"
+          action="https://69b99504-0bce-4fff-9de0-e95e1e36f9dc.mock.pstmn.io/banners"
+          listType="picture"
+          onChange={onChangeImage}
+        >
+          {imageUrl ? (
+            <img
+              src={`https://69b99504-0bce-4fff-9de0-e95e1e36f9dc.mock.pstmn.io/${imageUrl}`}
+            />
+          ) : (
+            <div>
+              <img src="/icons/camera.png" />
+              <span>이미지를 업로드 해주세요.</span>
+            </div>
+          )}
+        </Upload>
+        <Form.Item
+          label={<div className="upload-label">상세설명</div>}
+          name="Description"
+          rules={[{ required: true, message: "타이틀을 적어주세요" }]}
+        >
+          <Input placeholder="타이틀을 적어주세요" />
+        </Form.Item>
+        <div id="upload-button" size="large" htmlType="submit">
+          <Button danger>등록하기</Button>
+        </div>
+      </Form>
     </div>
   );
 }
 
-export default Uploadpage;
+export default UploadPage;
